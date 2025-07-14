b8828035e73c7db457758905bf2f8c64d00999f8
Someoneyeeting <mahmoudelshrif19@gmail.com>
2025-07-14 11:52:51 +0300

    oven animation

--- oven.gd
+++ oven.gd
@@ -3,6 +3,12 @@ class_name Oven
 
 
 func get_thrown(ids):
+	$Oven.play("cooking")
 	$close.play()
 	await get_tree().create_timer(0.2).timeout
 	$cooking.play()
+	$cooktime.start()
+
+
+func _on_cooktime_timeout() -> void:
+	$Oven.play("closed")
