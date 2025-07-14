b0ef8dcb4c7c4540f4e21af35ea9845cc36a5bbd
Someoneyeeting <mahmoudelshrif19@gmail.com>
2025-07-15 00:05:45 +0300

    oven timescale

--- oven.gd
+++ oven.gd
@@ -8,8 +8,16 @@ func get_thrown(ids):
 	$close.play()
 	await get_tree().create_timer(0.2).timeout
 	$cooking.play()
+	$Targetable/CollisionShape2D.disabled = true
 	$cooktime.start()
 
+func _physics_process(delta: float) -> void:
+	$cooking.pitch_scale = Engine.time_scale
+	if(Engine.time_scale < 1):
+		$cooking.volume_db = 16
+	else:
+		$cooking.volume_db = 24
 
 func _on_cooktime_timeout() -> void:
+	$Targetable/CollisionShape2D.disabled = false
 	$Oven.play("closed")
